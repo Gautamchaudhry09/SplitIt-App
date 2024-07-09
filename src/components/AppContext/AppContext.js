0b2d76f6816e9adf41d2b4d7a1f1fd467e8fba21
Gautam Chaudhry <100997330+Gautamchaudhry09@users.noreply.github.com>
2024-07-09 18:56:36 +0530

    Update AppContext.js

--- src/components/AppContext/AppContext.js
+++ src/components/AppContext/AppContext.js
@@ -1,40 +1,65 @@
-import { createContext, useEffect, useState } from "react";
-import React from "react";
+import { Box } from "@mui/material";
+import React, { useContext, useState } from "react";
+import { TabsContainer } from "./TabsContainer";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import RotateLeftIcon from "@mui/icons-material/RotateLeft";
+import { AccountContext } from "../AppContext/AppContext";
 
-export const AccountContext = createContext(null);
+export const MainContainer = () => {
+  const { setFriends, setTransactions } = useContext(AccountContext);
+  const [invert, setInvert] = useState(0);
 
-export const AccountProvider = ({ children }) => {
-  // Initialize state from local storage
-  const [friends, setFriends] = useState(() => {
-    const savedFriends = localStorage.getItem("friends");
-    return savedFriends ? JSON.parse(savedFriends) : [];
-  });
-
-  const [transactions, setTransactions] = useState(() => {
-    const savedTransactions = localStorage.getItem("transactions");
-    return savedTransactions ? JSON.parse(savedTransactions) : [];
-  });
-
-  // Use useEffect to save friends to local storage whenever it changes
-  useEffect(() => {
-    localStorage.setItem("friends", JSON.stringify(friends));
-  }, [friends]);
-
-  // Use useEffect to save transactions to local storage whenever it changes
-  useEffect(() => {
-    localStorage.setItem("transactions", JSON.stringify(transactions));
-  }, [transactions]);
+  const reset = () => {
+    if (
+      window.confirm(
+        "Are You Sure? All data will be reset and everything will be erased"
+      )
+    ) {
+      setFriends([]);
+      setTransactions([]);
+      localStorage.clear();
+    }
+  };
 
   return (
-    <AccountContext.Provider
-      value={{
-        friends,
-        setFriends,
-        transactions,
-        setTransactions,
+    <Box
+      sx={{
+        maxWidth: "450px",
+        mx: "auto",
+        my: "20px",
+        padding: "10px",
+        bgcolor: "grey",
+        border: "1px solid black",
+        overflowX: "hidden",
+        boxShadow: "3px 3px 10px black",
+        borderRadius: "10px",
       }}
     >
-      {children}
-    </AccountContext.Provider>
+      {invert == 0 ? (
+        <>
+          <LightModeIcon
+            sx={{ cursor: "pointer" }}
+            onClick={() => setInvert(1)}
+          />
+        </>
+      ) : (
+        <>
+          <Brightness4Icon
+            sx={{ cursor: "pointer" }}
+            onClick={() => setInvert(0)}
+          />
+        </>
+      )}
+      <RotateLeftIcon sx={{ cursor: "pointer" }} onClick={reset} />
+
+      <Box
+        sx={{
+          filter: `invert(${invert})`,
+        }}
+      >
+        <TabsContainer />
+      </Box>
+    </Box>
   );
 };
