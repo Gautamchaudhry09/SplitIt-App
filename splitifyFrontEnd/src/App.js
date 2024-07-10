ce3cb9be1a3c78004c4612639e20fa722108a30a
Gautam Chaudhry <100997330+Gautamchaudhry09@users.noreply.github.com>
2024-07-10 23:16:51 +0000

    added bg img

--- splitifyFrontEnd/src/App.js
+++ splitifyFrontEnd/src/App.js
@@ -1,25 +1,17 @@
-import { Divider } from "@mui/material";
+// App.js
 import "./App.css";
-import { AddFriends } from "./components/Add-friends/AddFriends";
-import { FriendList } from "./components/Add-friends/FriendList";
-import { Friends } from "./components/Add-friends/Friends";
 import { AccountProvider } from "./components/AppContext/AppContext";
-import { AddExpenses } from "./components/Add-expenses/AddExpenses";
-import { ExpensesList } from "./components/Add-expenses/ExpensesList";
-import { Expenses } from "./components/Add-expenses/Expenses";
-import { FinalTxns } from "./components/Final-transactions/FinalTxns";
-import { TabsContainer } from "./components/Containers/TabsContainer";
 import { MainContainer } from "./components/Containers/MainContainer";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { Login } from "./components/Pages/Login";
-import { Register } from "./components/Pages/Register";
-import { useEffect } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
 
 function App() {
   return (
     <AccountProvider>
+      <div className="background"></div>
       <Router>
-        <MainContainer />
+        <div className="app-content">
+          <MainContainer className="main-container" />
+        </div>
       </Router>
     </AccountProvider>
   );
