560f7347c980d22da9c2914703a190688345b423
Gautam Chaudhry <100997330+Gautamchaudhry09@users.noreply.github.com>
2024-07-10 22:42:23 +0000

    updated server url

--- splitifyFrontEnd/src/service/api.js
+++ splitifyFrontEnd/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { json } from "react-router-dom";
 
-const url = "http://localhost:8000";
+const url = "https://splitit-app.onrender.com";
 export const registerUser = async (data) => {
   try {
     await axios.post(`${url}/addUser`, data);
