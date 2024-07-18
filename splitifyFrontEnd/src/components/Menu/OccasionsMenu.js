97188af2ea8df1ae55cbd514c74b6651fa845759
Gautam Chaudhry <100997330+Gautamchaudhry09@users.noreply.github.com>
2024-07-18 11:25:24 +0530

    Update OccasionsMenu.js

--- splitifyFrontEnd/src/components/Menu/OccasionsMenu.js
+++ splitifyFrontEnd/src/components/Menu/OccasionsMenu.js
@@ -39,21 +39,21 @@ const StyledMenu = styled((props) => (
         : theme.palette.grey[300],
     boxShadow:
       "0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
-    "& .MuiMenu-list": {
-      padding: "0px 0",
-    },
+   // "& .MuiMenu-list": {
+ //     padding: "0px 0",
+//    },
     "& .MuiMenuItem-root": {
       "& .MuiSvgIcon-root": {
         fontSize: 18,
         color: theme.palette.text.secondary,
         marginRight: theme.spacing(1.5),
       },
-      "&:active": {
-        backgroundColor: alpha(
+   //   "&:active": {
+  //      backgroundColor: alpha(
    //       theme.palette.primary.main,
-          theme.palette.action.selectedOpacity
-        ),
-      },
+ //         theme.palette.action.selectedOpacity
+//        ),
+ //     },
     },
   },
 }));
